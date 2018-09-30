7b2e2e7888c31b2dfe92c853a5dc21be9f540aa6
Joao Paulo Trindade <tenoriojpaulo@gmail.com>
2018-09-30 16:16:10 -0300

    add TagView lib to renderize genres into DetailScreen

--- app/src/main/java/com/arctouch/codechallenge/ui/details/DetailsView.java
+++ app/src/main/java/com/arctouch/codechallenge/ui/details/DetailsView.java
@@ -1,12 +1,15 @@
 package com.arctouch.codechallenge.ui.details;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +17,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.arctouch.codechallenge.R;
+import com.arctouch.codechallenge.model.Genre;
 import com.arctouch.codechallenge.model.Movie;
 import com.arctouch.codechallenge.ui.home.HomeView;
 import com.arctouch.codechallenge.util.MovieImageUrlBuilder;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+
+import br.com.jpttrindade.tagview.DefaultTag;
+import br.com.jpttrindade.tagview.SimpleTag;
+import br.com.jpttrindade.tagview.TagView;
 
 public class DetailsView extends Fragment {
 
@@ -29,9 +37,10 @@ public class DetailsView extends Fragment {
     private TextView tvOverview;
     private ImageView ivPoster;
     private ImageView ivBackdrop;
-    private RecyclerView rvGenres;
-    private GenreAdapter genreAdapter;
     private TextView tvReleaseDate;
+    private TagView tagview;
+    private Toolbar toolbar;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
     public static DetailsView newInstance() {
         return new DetailsView();
@@ -54,12 +63,11 @@ public class DetailsView extends Fragment {
         ivBackdrop = view.findViewById(R.id.ivBackdrop);
 
         progressbar = view.findViewById(R.id.progressbar);
-        genreAdapter = new GenreAdapter();
 
-        rvGenres = view.findViewById(R.id.rvGenres);
-        rvGenres.setAdapter(genreAdapter);
-        rvGenres.setHasFixedSize(true);
-        rvGenres.setNestedScrollingEnabled(false);
+        tagview = view.findViewById(R.id.tagview);
+
+        toolbar = view.findViewById(R.id.toolbar);
+        collapsingToolbarLayout = view.findViewById(R.id.collapsing_toolbar_layout);
 
 
         return view;
@@ -68,12 +76,17 @@ public class DetailsView extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(view -> getActivity().onBackPressed());
+
         ViewCompat.setTransitionName(ivPoster, HomeView.SHARE_POSTER_IMAGE);
-        ViewCompat.setTransitionName(tvTitle, HomeView.SHARE_TITLE);
+       // ViewCompat.setTransitionName(tvTitle, HomeView.SHARE_TITLE);
 
 
         mViewModel = ViewModelProviders.of(this).get(DetailsViewModel.class);
         mViewModel.getMovieDetailLiveData().observe(this, movie -> setData(movie));
+
     }
 
 
@@ -84,7 +97,7 @@ public class DetailsView extends Fragment {
     }
 
     public void setData(Movie movie) {
-        tvTitle.setText(movie.title);
+        collapsingToolbarLayout.setTitle(movie.title);
         tvOverview.setText(movie.overview);
         tvReleaseDate.setText(movie.releaseDate);
         Glide.with(this)
@@ -96,11 +109,15 @@ public class DetailsView extends Fragment {
                 .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
                 .into(ivBackdrop);
 
-        genreAdapter.setGenres(movie.genres);
+        tagview.removeAll();
+        DefaultTag defaultTag;
+        for (Genre genre : movie.genres) {
+            defaultTag = new SimpleTag(genre.name, Color.parseColor("#9E9E9E"));
+            tagview.addTag(defaultTag);
+        }
 
         progressbar.setVisibility(View.GONE);
 
 
     }
-
 }
